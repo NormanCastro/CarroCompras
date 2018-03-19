967e935ff55e3fd05debf236329041a6988d17ad
Norman Castro <norman.castrof@gmail.com>
2018-03-19 09:33:56 -0300

    cartshopdone

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
   def index
 	  	if params[:buscar].present?
-	  		@products = Product.where('name like?', "%#{params[:buscar]}%")
+	  		consulta = params[:buscar].capitalize
+	  		@products = Product.where('name like?', "%#{consulta}%")
 
 	  	else
 	  	 	@products = Product.all
-	  end
- end 
-end
+	  	end
+ 	end
+end 
+
